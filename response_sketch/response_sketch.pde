7890420796389781fffd91d9cc3f236735997c20
Mel Huang <hello@melaniehuang.com>
2017-01-10 23:40:28 +1100

    sorting out lag

--- response_sketch/response_sketch.pde
+++ response_sketch/response_sketch.pde
@@ -1,1 +1,35 @@
 /*Recieve serial response from Arduino sketch and display arrow on screen*/
+
+import processing.serial.*;
+
+Serial myPort;
+
+void setup() {
+  size(800, 600);
+  background(255);
+
+  String danceMat = Serial.list()[1];
+  myPort = new Serial(this, danceMat, 9600);
+  myPort.bufferUntil('\n'); 
+
+  noStroke();
+  fill(200);
+  frameRate(60);
+}
+
+void draw() {
+  if ( myPort.available() > 0) {  
+    String val = myPort.readString();
+    if (val != null) { 
+      print(val);
+      
+      if (val.equals("H") == true) {
+        background(#A0FA3A);
+      } else if (val.equals("L") == true) {
+        background(0);
+      }
+    } else {
+      print("nothing here");
+    }
+  }
+}
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -2,33 +2,26 @@
 
 const int buttonPin = 3;     // the number of the pushbutton pin
 const int ledPin =  13;      // the number of the LED pin
-
-// variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   Serial.begin(9600);
-  // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
+  
   if (buttonState == HIGH) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
-    Serial.println("ON");
+    Serial.println("H");
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
-    Serial.println("OFF");
+    Serial.println("L");
   }
 
-  delay(100);
+  delay(200);
 }
