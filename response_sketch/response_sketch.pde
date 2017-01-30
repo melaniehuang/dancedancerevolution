7cfbb5b2f8f14f2fc3eab05e96e5ff20b1cf6d83
Mel Huang <hello@melaniehuang.com>
2017-01-30 14:14:47 +1100

    adding new sensors, note to self, don't daisychain the gnd on all pins - it will cause all sensors to be triggered when one is

--- response_sketch/response_sketch.pde
+++ response_sketch/response_sketch.pde
@@ -8,7 +8,7 @@ void setup() {
   size(displayWidth, displayHeight);
   background(255);
 
-  String danceMat = Serial.list()[1];
+  String danceMat = Serial.list()[2];
   myPort = new Serial(this, danceMat, 9600);
   myPort.bufferUntil('\n'); 
 
@@ -21,15 +21,18 @@ void draw() {
     String val = myPort.readString();
     
     if (val != null) { 
-      print(val.equals("H"));
+      print(val.equals("H1"));
       
-      if (val.equals("H") == true) {
+      if (val.equals("H1") == true) {
         background(#A0FA3A);
-      } else if (val.equals("L") == true) {
+      } else if (val.equals("L1") == true) {
         background(0);
-      }
-    } else {
-      print("nothing here");
+      } else if (val.equals("H2") == true) {
+        background(#FA1E1E);
+      } else if (val.equals("L2") == true) {
+        background(0);
+      } else {
+        print("nothing here");
     }
   }
 }
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,29 +1,49 @@
 /* Testing dance machine buttons to communicate to Processing */
 
-const int buttonPin = 3;     // the number of the pushbutton pin
+const int buttonPin1 = 3;     // the number of the pushbutton pin
+const int buttonPin2 = 4;
+const int buttonPin3 = 5;
 const int ledPin =  13;      // the number of the LED pin
-int buttonState = 0;         // variable for reading the pushbutton status
-
-String v = "";
+int buttonState1 = LOW;         // variable for reading the pushbutton status
+int buttonState2 = LOW;
 
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
 
-  if (buttonState == HIGH) {
-    // turn LED on:
+  if (buttonState1 == HIGH) {
     digitalWrite(ledPin, HIGH);
-    Serial.write("H");
-  } else {
-    // turn LED off:
+    Serial.write("1");
+  } else if (buttonState1 == LOW) {
     digitalWrite(ledPin, LOW);
-    Serial.write("L");
+    Serial.write("0");
   }
+
+  if (buttonState2 == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.write("2");
+  } else if (buttonState2 == LOW)   {
+    digitalWrite(ledPin, LOW);
+    Serial.write("0");
+  }
+
+  if (buttonState3 == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.write("3");
+  } else if (buttonState3 == LOW)   {
+    digitalWrite(ledPin, LOW);
+    Serial.write("0");
+  }
+  
   delay(100);
 }
 
